0ab1db2b9aedb1404db7a1bbcc3a09314f1b4126
Sandiwas <harrycode9@gmail.com>
2026-05-13 16:07:34 +0530

    read data

---
+++ src/test/java/Excel/E2ReadDataFromRow.java
@@ -0,0 +1,31 @@
+package Excel;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class E2ReadDataFromRow {
+	public static void main(String[] args) throws IOException {
+		FileInputStream fi = new FileInputStream(System.getProperty("user.dir") + "\\testData\\data.xlsx");
+		XSSFWorkbook wb = new XSSFWorkbook(fi);
+		XSSFSheet sheet = wb.getSheet("Sheet1");
+
+		int cells = sheet.getRow(0).getLastCellNum();
+
+		for (int i = 0; i < cells; i++) {
+			String data = sheet.getRow(0).getCell(i).toString();
+			System.out.println("Data from frest columne Cell " + i + " : " + data);
+		}
+		wb.close();
+		fi.close();
+	}
+}
+
+//Rows start form 0 (0-5)  ; getLastRowNum method  get last index so for (int i = 0; i <= rows; i++)
+//(row cell means column )coloum cell start from 1 (1-4); getLastcellNum method  get not last index so for (int i = 0; i < cell; i++)
+
+
+//getLastRowNum count start form 0 (i<=rows)
+//getLastcellNum  count start form  1  (i<cell)
