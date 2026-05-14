39b8ed0b897adc5bc60beb355b80d91ee2f034fa
Sandiwas <harrycode9@gmail.com>
2026-05-14 21:58:37 +0530

    read excel data

---
+++ src/test/java/Excel1/E3ReadDataFromColumn.java
@@ -0,0 +1,27 @@
+package Excel1;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class E3ReadDataFromColumn {
+	public static void main(String[] args) throws IOException {
+
+		FileInputStream fi = new FileInputStream(System.getProperty("user.dir") + "\\testData\\data.xlsx");
+		XSSFWorkbook wb = new XSSFWorkbook(fi);
+		XSSFSheet sheet = wb.getSheet("Sheet1");
+
+		int rows = sheet.getLastRowNum();
+		int cells = sheet.getRow(0).getLastCellNum();
+
+		for (int i = 0; i <= rows; i++) {
+			String data = sheet.getRow(i).getCell(0).toString();
+			System.out.println("Data from row Cell " + i + " : " + data);
+		}
+		System.out.println(cells);
+
+		wb.close();
+		fi.close();
+	}
+}
