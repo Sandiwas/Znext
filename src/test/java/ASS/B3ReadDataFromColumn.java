3989e9eddd8c316258f8ae4756a8b230bfdfa6fd
Sandiwas <harrycode9@gmail.com>
2026-05-31 23:21:08 +0530

    read data frome excel coloumn

---
+++ src/test/java/ASS/B3ReadDataFromColumn.java
@@ -0,0 +1,24 @@
+package ASS;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class B3ReadDataFromColumn {
+	public static void main(String[] args) throws IOException {
+		FileInputStream fi = new FileInputStream(".\\testData\\data.xlsx");
+		XSSFWorkbook wb = new XSSFWorkbook(fi);
+		XSSFSheet sheet = wb.getSheet("Sheet1");
+		int rows = sheet.getLastRowNum();
+		int cells = sheet.getRow(0).getLastCellNum();
+
+		for (int i = 0; i <= rows; i++) {
+			String data = sheet.getRow(i).getCell(0).getStringCellValue();
+			System.out.println(data);
+		}
+		System.out.println(cells);
+		wb.close();
+		fi.close();
+	}
+}
