107621cccc205be83a0f90aa9eaececae9a50c30
Sandiwas <harrycode9@gmail.com>
2026-06-25 23:16:51 +0530

    read data from excel

---
+++ src/test/java/Infosys/E4ReadDataFromAllRow.java
@@ -0,0 +1,31 @@
+package Infosys;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class E4ReadDataFromAllRow {
+	public static void main(String[] args) throws IOException {
+
+		FileInputStream fi = new FileInputStream(System.getProperty("user.dir") + "\\testData\\data.xlsx");
+		XSSFWorkbook wb = new XSSFWorkbook(fi);
+		XSSFSheet sheet = wb.getSheet("Sheet1");
+
+		int rows = sheet.getLastRowNum();
+		int cells = sheet.getRow(0).getLastCellNum();
+
+		for (int i = 0; i <= rows; i++) {
+			XSSFRow row = sheet.getRow(i);
+			for (int j = 0; j < cells; j++) {
+				String data = row.getCell(j).toString();
+				System.out.print(data+" ");
+			}
+			System.out.println();
+		}
+		wb.close();
+		fi.close();
+	}
+
+}
