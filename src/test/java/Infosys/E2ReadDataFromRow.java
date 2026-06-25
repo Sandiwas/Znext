1aab3f6cc8985434ab0985d4a344268ee6ecd417
Sandiwas <harrycode9@gmail.com>
2026-06-25 23:14:14 +0530

    read data from excel

---
+++ src/test/java/Infosys/E2ReadDataFromRow.java
@@ -0,0 +1,26 @@
+package Infosys;
+
+import java.io.FileInputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class E2ReadDataFromRow {
+
+	public static void main(String[] args) throws Exception {
+		FileInputStream fi = new FileInputStream(System.getProperty("user.dir") + "\\testData\\data.xlsx");
+		XSSFWorkbook wb = new XSSFWorkbook(fi);
+		XSSFSheet sheet = wb.getSheet("Sheet1");
+
+		int cells = sheet.getRow(0).getLastCellNum();
+
+		for (int i = 0; i < cells; i++) {
+			String data = sheet.getRow(0).getCell(i).getStringCellValue();
+
+			System.out.print(data + " ");
+		}
+		wb.close();
+		fi.close();
+	}
+
+}
